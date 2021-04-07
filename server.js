86f586deafac66a2c7fe5fdcacc26b612f1e6fe9
Jean-Charles Coudon <65890983+RealAcharnes@users.noreply.github.com>
2021-04-07 22:25:33 +0200

    La franchement je tient le bon bout

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+// Import dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-
+const path = require('path');
 
 //import routes
 const authRoutes = require('./routes/auth');
 const { db } = require('./models/User');
 
-// app
+// Create a new express application named 'app'
 const app = express();
 
 // Set our backend port to be either an environment variable or port 5000
@@ -25,6 +26,12 @@ mongoose
 })
     .then (() => console.log('Mongodb Database is Connected'))
 
+// This application level middleware prints incoming requests to the servers console, useful to see incoming requests
+app.use((req, res, next) => {
+    console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+    next();
+});
+
 // Configure the bodyParser middleware
 app.use(express.json());
 app.use(express.urlencoded({
@@ -36,6 +43,22 @@ app.use(cors());
 
 //routes middleware
 app.use('/api', authRoutes);
+
+// This middleware informs the express application to serve our compiled React files
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+};
+
+// Catch any bad requests
+app.get('*', (req, res) => {
+    res.status(200).json({
+        msg: 'Catch All'
+    });
+});
 
 app.listen(port, () => {
     console.log(`Welcome to the backend! Server is running on port ${port}`);
