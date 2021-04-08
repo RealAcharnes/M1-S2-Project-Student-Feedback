2b2f0058e242fe07d69d9b03dec536c6837ecdf6
Alice2021-G <78536900+Alice2021-G@users.noreply.github.com>
2021-04-08 13:28:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const { db } = require('./models/User');
 const app = express();
 
 // Set our backend port to be either an environment variable or port 5000
-const port = 5050;
-
+const port = process.env.PORT || 5050
 // connect mongodb database
 mongoose
     .connect("mongodb+srv://Project20:Project20password@cluster0.vhx6b.mongodb.net/project20?retryWrites=true&w=majority", {
