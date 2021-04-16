19f8e60f836a8156d170bf202724f1348c3e998d
kwaku-addo <kwakuaaddo473@gmail.com>
2021-04-16 17:27:38 +0200

    admintest

--- react.component.jsx
+++ react.component.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { InputGroup, Label } from '@admin-bro/design-system'
+import { InputGroup, Label, Box } from '@admin-bro/design-system'
 
 const MyReactComponent = props => {
   const { record, property } = props
-  const value = record.params[property.name]
+//   const value = record.params[property.name]
   return (
-    <InputGroup>
-      <Label>{property.name}</Label>
-      {value} [meters]
-    </InputGroup>
+    // <InputGroup>
+    //   <Label>{property.name}</Label>
+    //   {value} [meters]
+    // </InputGroup>
+    <div>
+        <h3>HIIIII</h3>
+        <Box>My custom dashboard</Box>
+    </div>
+
   )
 }
 export default MyReactComponent
