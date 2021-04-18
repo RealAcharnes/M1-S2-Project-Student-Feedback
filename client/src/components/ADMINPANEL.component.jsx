f1d06f584b13c42155f13f24d7fa5a7ead9d5d18
kwaku-addo <kwakuaaddo473@gmail.com>
2021-04-18 02:59:16 +0200

    new dependencies

--- client/src/components/ADMINPANEL.component.jsx
+++ client/src/components/ADMINPANEL.component.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
-import { Box } from '../../../.adminbro'
+import React, {useEffect, useState} from 'react'
+import { InputGroup, Label, Box } from '@admin-bro/design-system'
+import { ApiClient } from 'admin-bro'
 
-const Dashboard = (props) => {
-  return (
-    <h3>Hiiiiiiiiiii</h3>
-    // <Box>My custom dashboard</Box>
-  )
+
+
+const api = new ApiClient()
+
+const UserComponent = (props) => {
+const [data, setData] = useState({})
+const mystyle ={
+    background: "linear-gradient(to bottom, rgba(3,75, 82, 1) 0%, rgba(3,75, 82, 0) 100%)",
+    height: "100%",
+    margin: 0,
+    "background-repeat": "no-repeat",
+    "background-attachment": "fixed",
 }
 
-export default Dashboard
+const sidebar ={
+  background: '#f4f4f4',
+  margin: '5px',
+  padding: '10px 20px',
+  cursor: 'pointer',
+  // borderRadius: "10px"
+}
+
+  useEffect(() => {
+    api.getDashboard().then((response) => {
+      setData(response.data)
+    })
+  }, [])
+  const log = () => {
+      console.log(props)
+  }
+
+  return (
+      <div style={mystyle}>
+          <button onClick={()=>log()}>LOG</button>
+          <div className={`quiz`}>
+            <a href="/">
+            <span role="img" aria-label="about us">&#x1f481;&#x1f3fb;&#x200d;&#x2642;&#xfe0f;</span>
+            About us
+            </a>
+            <a href="/">
+            <span role="img" aria-label="price">&#x1f4b8;</span>
+            Pricing
+            </a>
+            <a href="/">
+            <span role="img" aria-label="contact">&#x1f4e9;</span>
+            Contact
+            </a>
+          </div>
+          <div className={`quiz`} style={sidebar}>
+              <h4>Cet étudiant n'a répondu à aucun quiz</h4>
+
+          </div>  
+          <div className={`quiz`} style={sidebar}>
+              <h4>Cet étudiant n'a répondu à aucun quiz</h4>
+
+          </div> 
+    </div>
+  )
+}
+export default UserComponent
